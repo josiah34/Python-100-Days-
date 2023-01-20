53fce9b6c574c60a531899b9ef2b5c1f80e5c60f
Josiah <josiah_g@live.com>
2023-01-20 12:38:11 -0500

    Create main.py
    Finished two digit addition code challenge

---
+++ two-digit-addition/main.py
@@ -0,0 +1,10 @@
+# Write a program that adds the digits in a 2-digit number
+
+
+
+two_digit_number = str(input("Type a two digit number: "))
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+
+print("The sum of the digits is: " + str(first_num + second_num))
